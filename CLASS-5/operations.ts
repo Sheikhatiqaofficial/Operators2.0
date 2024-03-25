be3614498bb8dd9e220d69757344afb1fc4f6653
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-03-25 22:55:17 +0500

    Add files via upload

--- CLASS-5/operations.ts
+++ CLASS-5/operations.ts
@@ -1,42 +1,44 @@
 
-// // MODULUS(%):
+// MODULUS(%):
 
-// let orange: number = 15
-// let bags: number = 2
-// let reminder: number = orange % bags;
-// console.log(reminder);
+let orange: number = 15
+let bags: number = 2
+let reminder: number = orange % bags;
+console.log(reminder);
 
-// // UNARY OPERATORS:
+// UNARY OPERATORS:
 
-// let a = 13 
-// let b = 14
+let a = 13 
+let b = 14
 
-// // (++):
+// (++):
 
-// console.log(a++);
-// console.log(++a);
-// console.log(b++);
-// console.log(++b);
+console.log(a++);
+console.log(++a);
+console.log(b++);
+console.log(++b);
 
-// // (--):
+// (--):
 
-// console.log(a--); 
-// console.log(--a); 
-// console.log(b--); 
-// console.log(--b);
+console.log(a--); 
+console.log(--a); 
+console.log(b--); 
+console.log(--b);
 
-// // HOME WORK:
+// HOME WORK:
 
-// let c: number = 5;
-// let d: number = 2;
-// let e: number = c = ++a + a++ + --b + b-- + a + b++ + b;
-// console.log(c);
+let c: number = 5;
+let d: number = 2;
+let e: number = c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
 
 
 let result = 3 + 4 * 5; // COMBINING OPERATORS:
 result++
 console.log(result);
+
 // ASSIGNMENT OPERATORS:
+
 let f = 3
 f += 2  // f is equal and + 1
 console.log(f);
@@ -50,7 +52,9 @@ f %= 2 // f is equal and % 1
 console.log(f);
 f **= 2 // f is equal and ** 1
 console.log(f);
+
 // COMPARISON OPERATORS:
+
 let g = 5
 let h = 2
 let isequal = (g==h); //false
@@ -59,7 +63,9 @@ let isgreaterthan = (g>h); //true
 let islessthan = (g<h);//false
 let greaterorequal = (g>=h); //true
 let lessorequal = (g<=h); //false
+
 // LOGICAL OPERATORS:
+
 let i = 5 
 let j = 2
 let logicAnd = (i > j) && (j < i); //agr ek bhi false hoga tu answer false hi ayega
